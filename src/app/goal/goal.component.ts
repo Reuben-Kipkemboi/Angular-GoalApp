cc961c17c607c6ba871bfde7ec7dbec78828fb7a
Reuben-Kipkemboi <reuben.kipkemboi@student.moringaschool.com>
2022-04-11 21:40:42 +0300

    ft :service - fetching of quote API

--- src/app/goal/goal.component.ts
+++ src/app/goal/goal.component.ts
@@ -1,52 +1,63 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { AlertService } from '../alert-service/alert.service';
 import { Goal } from '../goal';
-
+import { GoalService } from '../goal-service/goal.service';
+import { Quote } from '../quote-class/quote';
 @Component({
   selector: 'app-goal',
   templateUrl: './goal.component.html',
   styleUrls: ['./goal.component.css']
 })
 export class GoalComponent implements OnInit {
+  goals: Goal[];
+  alertService: AlertService;
+  quote: Quote;
 
-
-  goals: Goal[] = [
-    new Goal(1, 'Attend morning stand-ups', 'Wake up and prepare for morning stand ups',new Date(2022,4,6)),
-    new Goal(2,'Cover days content','Cover the days content and ask for assistance',new Date(2022,4,7)),
-    new Goal(3,'Attend Evening Checkouts','Every evening attend the Checkouts on time',new Date(2022,4,8)),
-    new Goal(4,'Work on Ips during the weekend','Work on good and quality IPs',new Date(2022,4,9)),
-    new Goal(5,'Attend the Fridays feedbacks ','Give both positive and Negative feedbacks',new Date(2022,4,10)),
-    new Goal(6,'Relax','Find time to relax on sundays to reenergize for a new week',new Date(2022,4,11)),
-
-    
-  ];
-
-  toggleDetails(index){
-    this.goals[index].showDescription = !this.goals[index].showDescription;
-  }
-  // completeGoal(isComplete, index){
-  //   if (isComplete) {
-  //     this.goals.splice(index,1);
-  //   }
-  // }
-  deleteGoal(isComplete, index){
-    if (isComplete) {
-      let toDelete = confirm(`Are you sure you want to delete ${this.goals[index].name}?`)
-
-      if (toDelete){
-        this.goals.splice(index,1)
-      }
-    }
-  }
-  addNewGoal(goal){
+  // goals: any;
+  addNewGoal(goal) {
     let goalLength = this.goals.length;
-    goal.id = goalLength+1;
+    goal.id = goalLength + 1;
     goal.completeDate = new Date(goal.completeDate)
     this.goals.push(goal)
   }
-  
-  constructor() { }
+  toggleDetails(index) {
+    this.goals[index].showDescription = !this.goals[index].showDescription;
+  }
+  completeGoal(isComplete, index) {
+    if (isComplete) {
+      this.goals.splice(index, 1);
+    }
+  }
+  deleteGoal(isComplete, index) {
+    if (isComplete) {
+      let toDelete = confirm(`Are you sure you want to delete ${this.goals[index].name}?`)
 
-  ngOnInit(): void {
+      if (toDelete) {
+        this.goals.splice(index, 1)
+        this.alertService.alertMe("This goal has been trashed and deleted make another goal")
+      }
+    }
   }
 
+  constructor(goalService: GoalService, alertService: AlertService, private http: HttpClient) {
+    this.goals = goalService.getGoals()
+    this.alertService = alertService;
+  }
+
+  ngOnInit(): void {
+    this.http.get<ApiResponse>("http://quotes.stormconsultancy.co.uk/random.json").subscribe(data=>{
+      // Succesful API request
+      this.quote = new Quote(data.author, data.quote)
+    },err=>{
+        this.quote = new Quote("Winston Churchill","Never never give up!")
+        console.log("An error occurred")
+    })
+    interface ApiResponse {
+      author: string;
+      quote: string;
+    }
+  }
 }
+
+
---
+++ src/app/goalList.ts
@@ -0,0 +1,13 @@
+import { Goal } from './goal';
+
+export let goals: Goal[] = [
+    new Goal(1, 'Attend morning stand-ups', 'Wake up and prepare for morning stand ups',new Date(2022,4,6)),
+    new Goal(2,'Cover days content','Cover the days content and ask for assistance',new Date(2022,4,7)),
+    new Goal(3,'Attend Evening Checkouts','Every evening attend the Checkouts on time',new Date(2022,4,8)),
+    new Goal(4,'Work on Ips during the weekend','Work on good and quality IPs',new Date(2022,4,9)),
+    new Goal(5,'Attend the Fridays feedbacks ','Give both positive and Negative feedbacks',new Date(2022,4,10)),
+    new Goal(6,'Relax','Find time to relax on sundays to reenergize for a new week',new Date(2022,4,11)),
+
+    
+  ];
+
